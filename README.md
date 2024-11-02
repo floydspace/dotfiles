ae6a38ad0e35a2a5c4bcafce24538d216d148564
Victor Korzunin <victor.korzunin@nn.nl>
2024-11-02 18:08:48 +0100

    feat: add README and update nix-darwin configuration with new packages and defaults

---
+++ README.md
@@ -0,0 +1,5 @@
+How to apply nix-darwin configuration:
+
+```sh
+darwin-rebuild switch --flake "./nix-darwin#default"
+```
--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -13,10 +13,12 @@
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
+      nixpkgs.config.allowUnfree = true;
       environment.systemPackages =
         [ 
           pkgs.alacritty
           pkgs.iterm2
+          pkgs.slack
           pkgs.vim
         ];
 
@@ -35,8 +37,8 @@
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true; # default shell on catalina
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -44,6 +46,14 @@
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
       system.stateVersion = 5;
+
+      system.defaults = {
+        dock.autohide = false;
+        finder.FXPreferredViewStyle = "Nlsv";
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+        NSGlobalDomain.AppleShowAllExtensions = true;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+      };
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "x86_64-darwin";
